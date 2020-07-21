f27286573346843ff266e13278d353290c658ae7
corymhall <43035978+corymhall@users.noreply.github.com>
2020-07-21 13:26:22 +0000

    updating region for prod account

--- sample-app/bin/sample-app.ts
+++ sample-app/bin/sample-app.ts
@@ -6,7 +6,7 @@ import { SampleApp } from '../lib/sample-app';
 const app = new cdk.App();
 
 const dev = { account: 'REPLACE_WITH_DEV_ACCOUNT_ID', region: 'us-east-2' }
-const prod = { account: 'REPLACE_WITH_PROD_ACCOUNT_ID', region: 'us-east-2' }
+const prod = { account: 'REPLACE_WITH_PROD_ACCOUNT_ID', region: 'us-east-1' }
 
 new SampleApp(app, 'devSampleApp', { env: dev });
 new SampleApp(app, 'prodSampleApp', { env: prod });
